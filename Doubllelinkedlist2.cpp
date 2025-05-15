febf9fa3358c6724ec129fcf96e65609e01c6d56
Yuratama1 <yuratama661@gmail.com>
2025-05-15 15:16:31 +0700

    membuat double linkedlist ke public

--- Doubllelinkedlist2.cpp
+++ Doubllelinkedlist2.cpp
@@ -12,4 +12,17 @@ class Node{
 class DoubleLinkedList{
     private:
     Node *START;
+
+    public:
+        DoubleLinkedList(){
+            START = NULL;
+        }
+        void addNode(){
+            int nim;
+            cout << "\nEnter the roll number of the student: ";
+            cin >> nim;
+
+            //step 1 allocation mememory of new node
+            Node *newNode = new Node();
+        }
 };
